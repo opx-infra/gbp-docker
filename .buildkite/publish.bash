7d0ca9bed6391bb39a93931e4b3e27c6c64e7fc4
Tyler Heucke <tyler.heucke@dell.com>
2018-08-29 18:47:21 -0700

    Only publish tagged image on tags
    Signed-off-by: Tyler Heucke <tyler.heucke@dell.com>

--- .buildkite/publish.bash
+++ .buildkite/publish.bash
@@ -2,26 +2,24 @@
 set -euo pipefail
 #set -x
 
-if [[ "$BUILDKITE_BRANCH" == "master" ]] || [[ -n "$BUILDKITE_TAG" ]]; then
-  echo "+++ Publishing images"
-else
-  exit 0
+if [[ "$BUILDKITE_BRANCH" == "master" ]]; then
+  echo "+++ Publishing stable images"
+  docker push "opxhub/gbp:${DIST}"
+  docker push "opxhub/gbp:${DIST}-dev"
+elif [[ -n "$BUILDKITE_TAG" ]]; then
+  shopt -s extglob
+  case "$BUILDKITE_TAG" in
+    v*.*.*) echo "+++ Publishing release $BUILDKITE_TAG";;
+    *) echo "--- Skipping tag $BUILDKITE_TAG"; exit 0;;
+  esac
+
+  docker tag  "opxhub/gbp:${DIST}" \
+              "opxhub/gbp:${BUILDKITE_TAG}-${DIST}"
+  docker push "opxhub/gbp:${BUILDKITE_TAG}-${DIST}"
+  docker pull "opxhub/gbp:${DIST}"
+
+  docker tag  "opxhub/gbp:${DIST}-dev" \
+              "opxhub/gbp:${BUILDKITE_TAG}-${DIST}-dev"
+  docker push "opxhub/gbp:${BUILDKITE_TAG}-${DIST}-dev"
+  docker pull "opxhub/gbp:${DIST}-dev"
 fi
-
-docker push "opxhub/gbp:${DIST}"
-docker push "opxhub/gbp:${DIST}-dev"
-
-if [[ -z "$BUILDKITE_TAG" ]]; then
-  exit 0
-fi
-
-shopt -s extglob
-case "$BUILDKITE_TAG" in
-  v*.*.*) echo "+++ Publishing release $BUILDKITE_TAG";;
-  *) echo "--- Skipping tag $BUILDKITE_TAG"; exit 0;;
-esac
-
-docker tag "opxhub/gbp:${DIST}" "opxhub/gbp:${BUILDKITE_TAG}-${DIST}"
-docker tag "opxhub/gbp:${DIST}-dev" "opxhub/gbp:${BUILDKITE_TAG}-${DIST}-dev"
-docker push "opxhub/gbp:${BUILDKITE_TAG}-${DIST}"
-docker push "opxhub/gbp:${BUILDKITE_TAG}-${DIST}-dev"
