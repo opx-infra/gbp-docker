4fee401ae1ca639321e45b22f5a46e4b47b7d76d
Tyler Heucke <tyler.heucke@dell.com>
2018-08-29 18:21:35 -0700

    Release on tag with version
    Signed-off-by: Tyler Heucke <tyler.heucke@dell.com>

--- .buildkite/publish.bash
+++ .buildkite/publish.bash
@@ -2,10 +2,20 @@
 set -euo pipefail
 #set -x
 
-docker tag "opxhub/gbp:$DIST" "opxhub/gbp:$(git rev-parse HEAD)-${DIST}"
-docker tag "opxhub/gbp:${DIST}-dev" "opxhub/gbp:$(git rev-parse HEAD)-${DIST}-dev"
-
 docker push "opxhub/gbp:${DIST}"
-docker push "opxhub/gbp:$(git rev-parse HEAD)-${DIST}"
 docker push "opxhub/gbp:${DIST}-dev"
-docker push "opxhub/gbp:$(git rev-parse HEAD)-${DIST}-dev"
+
+if [[ -z "$BUILDKITE_TAG" ]]; then
+  exit 0
+fi
+
+shopt -s extglob
+case "$BUILDKITE_TAG" in
+  v*.*.*) echo "+++ Publishing release $BUILDKITE_TAG";;
+  *) echo "--- Skipping tag $BUILDKITE_TAG"; exit 0;;
+esac
+
+docker tag "opxhub/gbp:${DIST}" "opxhub/gbp:${BUILDKITE_TAG}-${DIST}"
+docker tag "opxhub/gbp:${DIST}-dev" "opxhub/gbp:${BUILDKITE_TAG}-${DIST}-dev"
+docker push "opxhub/gbp:${BUILDKITE_TAG}-${DIST}"
+docker push "opxhub/gbp:${BUILDKITE_TAG}-${DIST}-dev"
