627138101b7ea0c0d7e12c2e856eaad6c9a5f2d0
Tyler Heucke <tyler@heucke.io>
2018-10-08 12:58:59 -0700

    Add Ubuntu Dockerfiles
    Signed-off-by: Tyler Heucke <tyler@heucke.io>

--- update.sh
+++ update.sh
@@ -2,17 +2,31 @@
 set -euo pipefail
 #set -x
 
+update() {
+  os="$1"
+  dist="$2"
+
+  mkdir -p "$os/$dist/base"
+  sed -r \
+      -e 's!%%OS%%!'"$os"'!g' \
+      -e 's!%%DIST%%!'"$dist"'!g' \
+      -e 's!%%ARCH%%!amd64!g' \
+      "Dockerfile-base.template" >"$os/$dist/base/Dockerfile"
+  sed -r \
+      -e 's!%%OS%%!'"$os"'!g' \
+      -e 's!%%DIST%%!'"$dist"'!g' \
+      -e 's!%%ARCH%%!amd64!g' \
+      "Dockerfile-dev.template" >"$os/$dist/Dockerfile"
+}
+
+os=debian
 debians=( stretch buster )
 for dist in "${debians[@]}"; do
-  mkdir -p "debian/$dist/base"
-  sed -r \
-      -e 's!%%OS%%!debian!g' \
-      -e 's!%%DIST%%!'"$dist"'!g' \
-      -e 's!%%ARCH%%!amd64!g' \
-      "Dockerfile-base.template" >"debian/$dist/base/Dockerfile"
-  sed -r \
-      -e 's!%%OS%%!debian!g' \
-      -e 's!%%DIST%%!'"$dist"'!g' \
-      -e 's!%%ARCH%%!amd64!g' \
-      "Dockerfile-dev.template" >"debian/$dist/Dockerfile"
+  update "$os" "$dist"
+done
+
+os=ubuntu
+ubuntus=( xenial bionic )
+for dist in "${ubuntus[@]}"; do
+  update "$os" "$dist"
 done
